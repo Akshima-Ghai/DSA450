ab1296fd26d091666d206bcc425b2927b20f1f05
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-08 14:25:40 +0530

    Create string-Palindrome.cpp

---
+++ string-Palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:	
+	
+	
+	int isPlaindrome(string S)
+	{
+	    string rev_str;
+	    int len = S.length();
+	    for(int i=len-1;i>=0;i--){
+	        rev_str.push_back(S[i]);
+	    }
+	    if(S==rev_str){
+	        return 1;
+	    }else{
+	        return 0;
+	    }
+	}
+
+};
